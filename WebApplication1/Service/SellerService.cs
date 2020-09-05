64ea649435012d5b0266b562795cc78d58f27f36
Matheus Hoesel Dias <matheushoeseldias@gmail.com>
2020-09-05 15:42:59 -0300

    Delete seller

--- WebApplication1/Service/SellerService.cs
+++ WebApplication1/Service/SellerService.cs
@@ -26,5 +26,17 @@ namespace WebApplication1.Service
             _context.SaveChanges();
         }
 
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
+
     }
 }
